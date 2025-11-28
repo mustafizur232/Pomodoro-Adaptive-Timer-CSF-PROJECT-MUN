c2683d25fcbc1ddc353e7563f5e3a4946795b699
Mustafizur Rahman <mustafizur232@gmail.com>
2025-11-28 01:34:59 -0330

    sample time limit and load settings initialized

--- Promodo_Timer/pomodoro_timer.py
+++ Promodo_Timer/pomodoro_timer.py
@@ -4,3 +4,32 @@ import os
 
 SETTINGS_FILE = "pomodoro_settings.json"
 
+# Some simple limits so things don't get crazy
+MIN_WORK_MINUTES = 10
+MAX_WORK_MINUTES = 60
+MIN_BREAK_MINUTES = 3
+MAX_BREAK_MINUTES = 20
+MAX_RATING_HISTORY = 10  # how many past ratings we consider
+
+
+def load_settings():
+    """
+    Load work/break minutes and rating history from a JSON file.
+    If the file doesn't exist or is invalid, use default values.
+    """
+    default_settings = {
+        "work_minutes": 25,
+        "break_minutes": 5,
+        "ratings": []  # list of past productivity ratings (1–5)
+    }
+
+    if not os.path.exists(SETTINGS_FILE):
+        return default_settings
+
+    try:
+        with open(SETTINGS_FILE, "r") as f:
+            data = json.load(f)
+
+        work = int(data.get("work_minutes", 25))
+        brk = int(data.get("break_minutes", 5))
+        ratings = data.get("ratings", [])
